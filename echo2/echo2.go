c9a35d4ac5e97a117a50685be99b83945b0c62f8
jgmontoya <jgmontoya@uc.cl>
2018-09-22 16:22:44 +0900

    Comments

--- echo2/echo2.go
+++ echo2/echo2.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	s, sep := "", ""
-	for _, arg := range os.Args[1:] { // range returns index and element
+	s, sep := "", ""                  // Short variable declaration can only be used inside a function.
+	for _, arg := range os.Args[1:] { // range returns index and element, _ is used to discard unused values.
 		s += sep + arg
 		sep = " "
 	}
