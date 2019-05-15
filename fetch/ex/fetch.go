eab45278e31a40cad011632e09ee6519bac748f0
jgmontoya <jgmontoya@uc.cl>
2019-05-15 16:59:00 +0900

    Housekeeping :shower:

--- fetch/ex/fetch.go
+++ fetch/ex/fetch.go
@@ -3,24 +3,30 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		fmt.Printf("%s responded with status code: %s\n", url, resp.Status)
+		//b, err := ioutil.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body) // returns number of bytes copied and the first error encountered (if any)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		//fmt.Printf("%s", b)
 	}
 }
